f7e52501b06a85112fe02a7c3e0217fca906d9bb
buoi1 <PMT201@PMT3015>
2026-03-06 11:22:46 +0700

    buoi111

---
+++ buoi11/buoi1/buoi1.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+struct sinhvien {
+	string hoten;
+	int tuoi;
+	bool gioitinh;
+	string diachi;
+};
+int main() {  
+	sinhvien b;
+	b.hoten = "nguyen van A";
+	b.tuoi = 19;
+	b.gioitinh = true;
+	b.diachi = "argentina";
+
+		
+	sinhvien c;
+	c.hoten = "Anh Si";
+	c.tuoi = 20; 
+
+	vector<sinhvien>mang;
+	mang.push_back(b);
+	mang.push_back(c);
+	for (int i = 0;i < mang.size();i++) {
+		 cout << "sinh vien" << (i + 1) << endl;
+		 cout<<""
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int a = 772;
+	//int* p = &a;
+	//a += 2929;
+	//a = *p + 2;
+	//cout << *p;
+
+	/*vector<float>diemthi;
+	diemthi.push_back(5.55);
+	diemthi.push_back(5.33);
+	diemthi.push_back(5.5);
+	diemthi.pop_back(); 
+	for (int i = 0;i < diemthi.size();i++) {
+		cout << diemthi[i] << endl;
+	}*/
+
+}
